a59b347c97215ef6c956269fa99780238a820887
unknown <rfparsons@dmacc.edu>
2022-02-17 20:47:48 -0600

    Added ability to add and view games on web

---
+++ src/controller/AddGameServlet.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Game;
+
+/**
+ * Servlet implementation class AddGameServlet
+ */
+@WebServlet("/addGameServlet")
+public class AddGameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddGameServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String title = request.getParameter("title");
+		String publisher = request.getParameter("publisher");
+		String platform = request.getParameter("platform");
+		int year = Integer.parseInt(request.getParameter("year"));
+		
+		Game gm = new Game(title, publisher, platform, year);
+		GameHelper gh = new GameHelper();
+		gh.insertGame(gm);
+		
+		getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+	}
+
+}
